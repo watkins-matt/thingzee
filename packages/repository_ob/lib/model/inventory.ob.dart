b1694256c86b72086070db7371e89327dfe26f05
Matt Watkins <48530805+watkins-matt@users.noreply.github.com>
2023-06-08 09:59:22 -0700

    Regenerate and move ObjectBox code

--- packages/repository_ob/lib/model/inventory.ob.dart
+++ packages/repository_ob/lib/model/inventory.ob.dart
@@ -1,11 +1,9 @@
-import 'dart:convert';
 import 'dart:core';
 
 import 'package:objectbox/objectbox.dart';
 import 'package:quiver/core.dart';
-import 'package:repository/model/history.dart';
+import 'package:repository/ml/ml_history.dart';
 import 'package:repository/model/inventory.dart';
-
 @Entity()
 class ObjectBoxInventory {
   late double amount;
@@ -13,7 +11,8 @@ class ObjectBoxInventory {
   late Optional<DateTime> lastUpdate;
   List<DateTime> expirationDates = [];
   List<String> locations = [];
-  late History history;
+  @Transient()
+  late MLHistory history;
   late bool restock;
   @Unique()
   late String upc;
@@ -45,46 +44,7 @@ class ObjectBoxInventory {
       ..restock = restock
       ..upc = upc
       ..iuid = iuid
-      ..units = units;
-  }
-
-  String get dbHistory {
-    return jsonEncode(history.toJson());
-  }
-
-  set dbHistory(String value) {
-    Map<String, dynamic> json = jsonDecode(value) as Map<String, dynamic>;
-    history = History.fromJson(json).trim();
-  }
-
-  int get dbLastUpdate {
-    return lastUpdate.isPresent ? lastUpdate.value.millisecondsSinceEpoch : 0;
-  }
-
-  set dbLastUpdate(int value) {
-    lastUpdate = value != 0
-        ? Optional.of(DateTime.fromMillisecondsSinceEpoch(value))
-        : const Optional.absent();
-  }
-
-  List<String> get dbExpirationDates {
-    List<String> dates = [];
-    for (final exp in expirationDates) {
-      dates.add(exp.millisecondsSinceEpoch.toString());
-    }
-
-    return dates;
-  }
-
-  set dbExpirationDates(List<String> dates) {
-    expirationDates.clear();
-
-    for (final date in dates) {
-      int? timestamp = int.tryParse(date);
-
-      if (timestamp != null) {
-        expirationDates.add(DateTime.fromMillisecondsSinceEpoch(timestamp));
-      }
-    }
+      ..units = units
+    ;
   }
 }
