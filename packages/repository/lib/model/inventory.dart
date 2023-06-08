a559413057cc27180dff03633218c17421ae6a11
Matt Watkins <48530805+watkins-matt@users.noreply.github.com>
2023-06-08 09:58:37 -0700

    Use the new MLHistory

--- packages/repository/lib/model/inventory.dart
+++ packages/repository/lib/model/inventory.dart
@@ -1,7 +1,7 @@
+import 'package:intl/intl.dart';
 import 'package:quiver/core.dart';
+import 'package:repository/ml/ml_history.dart';
 import 'package:stats/double.dart';
-
-import 'history.dart';
 
 class Inventory {
   Inventory();
@@ -11,7 +11,7 @@ class Inventory {
   Optional<DateTime> lastUpdate = const Optional.absent();
   List<DateTime> expirationDates = <DateTime>[];
   List<String> locations = <String>[];
-  History history = History();
+  MLHistory history = MLHistory();
   bool restock = true;
 
   // Reference to Item
@@ -26,11 +26,6 @@ class Inventory {
     assert(unitCount != 0);
     amount = value / unitCount;
   }
-
-  // set dbHistory(String value) {
-  //   Map<String, dynamic> json = jsonDecode(value) as Map<String, dynamic>;
-  //   history = History.fromJson(json).trim();
-  // }
 
 //   int get dbLastUpdate {
 //     return lastUpdate.isPresent ? lastUpdate.value.millisecondsSinceEpoch : 0;
@@ -92,18 +87,13 @@ class Inventory {
 //         : (1 / history.currentSeriesRegression.abs()) / 1000 / 60 / 60 / 24;
 //   }
 
-//   @override
-//   int compareTo(Product other) {
-//     return name.compareTo(other.name);
-//   }
-
-  bool get canPredictAmount {
-    return lastUpdate.isPresent && history.hasIntercept;
+  bool get canPredict {
+    return lastUpdate.isPresent && history.canPredict;
   }
 
-//   bool get isPredictedOut {
-//     return predictedAmount <= 0;
-//   }
+  bool get isPredictedOut {
+    return predictedAmount <= 0;
+  }
 
 //   String get minutesToReduceByOneString {
 //     final reductionInMinutes = Duration(minutes: usageSpeedMinutes.round());
@@ -115,49 +105,50 @@ class Inventory {
 
   double get predictedAmount {
     // If we can't predict anything, return the existing amount
-    if (!canPredictAmount) return amount;
-    double predictedQuantity = history.predict(DateTime.now().millisecondsSinceEpoch.toDouble());
+    if (!canPredict) return amount;
+    double predictedQuantity = history.predict(DateTime.now().millisecondsSinceEpoch);
     return predictedQuantity > 0 ? predictedQuantity.toDouble() : 0;
   }
 
-//   DateTime get predictedOutDate {
-//     assert(canPredictAmount);
-//     // if (history.xIntercept.isNaN) {
-//     //   var val = history.xIntercept;
-//     //   val += 1;
-//     // }
+  DateTime get predictedOutDate {
+    // Predicted out date is undefined. Code should be checking
+    // canPredict before using this value.
+    if (!canPredict) {
+      return DateTime.fromMillisecondsSinceEpoch(0);
+    }
 
-//     return DateTime.fromMillisecondsSinceEpoch(history.xIntercept.round());
-//   }
+    return DateTime.fromMillisecondsSinceEpoch(history.predictedOutageTimestamp);
+  }
 
-//   String get predictedOutDateString {
-//     return canPredictAmount
-//         ? DateFormat.yMd().add_jm().format(predictedOutDate)
-//         : 'No product history available to make predictions.';
-//   }
+  String get predictedOutDateString {
+    return canPredict
+        ? DateFormat.yMd().add_jm().format(predictedOutDate)
+        : 'No product history available to make predictions.';
+  }
 
-//   Duration get predictedTimeUntilOut {
-//     if (!canPredictAmount) {
-//       return const Duration(milliseconds: 0);
-//     }
+  Duration get predictedTimeUntilOut {
+    if (!canPredict) {
+      return const Duration(milliseconds: 0);
+    }
 
-//     double millisecondsUntilOut = history.xIntercept - DateTime.now().millisecondsSinceEpoch;
-//     return Duration(milliseconds: millisecondsUntilOut.round().abs());
-//   }
+    int millisecondsUntilOut =
+        history.predictedOutageTimestamp - DateTime.now().millisecondsSinceEpoch;
+    return Duration(milliseconds: millisecondsUntilOut.abs());
+  }
 
-//   String get predictedTimeUntilOutString {
-//     assert(canPredictAmount);
-//     final alreadyGoneString =
-//         '${'Item was gone ' + predictedTimeUntilOut.toHumanReadableString()} ago.';
-//     final timeUntilGoneString =
-//         '${'${'Item will be gone in ' + predictedTimeUntilOut.toHumanReadableString()}\nat ${DateFormat.yMd().add_jm().format(predictedOutDate)}'}.';
+  // String get predictedTimeUntilOutString {
+  //   assert(canPredict);
+  //   final alreadyGoneString =
+  //       '${'Item was gone ' + predictedTimeUntilOut.toHumanReadableString()} ago.';
+  //   final timeUntilGoneString =
+  //       '${'${'Item will be gone in ' + predictedTimeUntilOut.toHumanReadableString()}\nat ${DateFormat.yMd().add_jm().format(predictedOutDate)}'}.';
 
-//     return isPredictedOut ? alreadyGoneString : timeUntilGoneString;
-//   }
+  //   return isPredictedOut ? alreadyGoneString : timeUntilGoneString;
+  // }
 
-//   double get predictedUnits {
-//     return predictedAmount * unitCount;
-//   }
+  double get predictedUnits {
+    return predictedAmount * unitCount;
+  }
 
   double get preferredAmount {
     return unitCount == 1 ? amount : units;
@@ -167,9 +158,9 @@ class Inventory {
     return preferredAmount.roundTo(2).toString();
   }
 
-  // String get preferredPredictedUnitString {
-  //   return canPredictAmount
-  //       ? predictedUnits.toStringAsFixed(2).toString()
-  //       : units.toStringAsFixed(2).toString();
-  // }
+  String get preferredPredictedUnitString {
+    return canPredict
+        ? predictedUnits.toStringAsFixed(2).toString()
+        : units.toStringAsFixed(2).toString();
+  }
 }
