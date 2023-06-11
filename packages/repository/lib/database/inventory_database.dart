a33d27dfa4dd74040417fa8da85d9aa6c01b7dde
Matt Watkins <48530805+watkins-matt@users.noreply.github.com>
2023-06-11 16:57:14 -0700

    Sorted members

--- packages/repository/lib/database/inventory_database.dart
+++ packages/repository/lib/database/inventory_database.dart
@@ -2,10 +2,10 @@ import 'package:quiver/core.dart';
 import 'package:repository/model/inventory.dart';
 
 abstract class InventoryDatabase {
-  Map<String, Inventory> map();
   List<Inventory> all();
   void delete(Inventory inv);
   void deleteAll();
   Optional<Inventory> get(String upc);
+  Map<String, Inventory> map();
   void put(Inventory inv);
 }
