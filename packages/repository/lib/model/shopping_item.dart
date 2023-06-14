0e9b1d4271221c59c1eabe06be98fc0048c91687
Matt Watkins <48530805+watkins-matt@users.noreply.github.com>
2023-06-14 13:59:37 -0700

    Add item reference

--- packages/repository/lib/model/shopping_item.dart
+++ packages/repository/lib/model/shopping_item.dart
@@ -1,9 +1,15 @@
+import 'package:repository/model/item.dart';
+
 class ShoppingListItem {
   String upc = '';
   bool checked = false;
+
+  Item item = Item();
 }
 
 class ShoppingCartItem {
   String upc = '';
   int price = 0;
+
+  Item item = Item();
 }
