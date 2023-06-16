fc79f3c0cbfa689cc7c0221c57f7420ba7d4f817
Matt Watkins <48530805+watkins-matt@users.noreply.github.com>
2023-06-16 09:02:36 -0700

    Initial remote backend abstract class

--- packages/repository/lib/repository.dart
+++ packages/repository/lib/repository.dart
@@ -4,8 +4,17 @@ import 'package:repository/database/item_database.dart';
 
 abstract class Repository {
   bool ready = false;
+  bool get isMultiUser => false;
 
   late ItemDatabase items;
   late InventoryDatabase inv;
   late HistoryDatabase hist;
+}
+
+abstract class SharedRepository extends Repository {
+  @override
+  bool get isMultiUser => true;
+
+  Future<void> registerUser(String username, String password);
+  Future<void> loginUser(String username, String password);
 }
