06f44d77bc57e32c0bd71d59ed50e6bf3b8f1aa5
Matt Watkins <48530805+watkins-matt@users.noreply.github.com>
2023-06-17 16:35:35 -0700

    Hide debug banner

--- lib/app.dart
+++ lib/app.dart
@@ -20,6 +20,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Thingzee',
       theme: ThemeData(
         useMaterial3: true,
