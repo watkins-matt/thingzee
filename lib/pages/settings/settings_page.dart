ec5e7f5eea77bbe4c560c22d0e127660a1373b53
Matt Watkins <48530805+watkins-matt@users.noreply.github.com>
2023-06-17 16:33:40 -0700

    Remove legacy code

--- lib/pages/settings/settings_page.dart
+++ lib/pages/settings/settings_page.dart
@@ -1,17 +1,11 @@
-import 'dart:io';
-
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:quiver/core.dart';
 import 'package:settings_ui/settings_ui.dart';
-import 'package:share_plus/share_plus.dart';
 import 'package:thingzee/app.dart';
 import 'package:thingzee/data/csv_export_service.dart';
 import 'package:thingzee/data/csv_import_service.dart';
-import 'package:thingzee/data/legacy_csv_exporter.dart';
-import 'package:thingzee/data/legacy_csv_importer.dart';
 import 'package:thingzee/pages/inventory/state/inventory_view.dart';
 import 'package:thingzee/pages/inventory/state/item_thumbnail_cache.dart';
 
@@ -77,144 +71,6 @@ class _SettingsPageState extends ConsumerState<SettingsPage> {
     ));
   }
 
-  Future<void> onImportHistory(BuildContext context) async {
-    final filePath = await pickFilePath();
-
-    if (filePath.isPresent) {
-      final file = File(filePath.value);
-      final contents = await file.readAsString();
-
-      await LegacyCsvImporter.importHistory(contents, App.repo);
-
-      final view = ref.read(inventoryProvider.notifier);
-      await view.refresh();
-
-      if (!mounted) return;
-      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-        content: Text('Backup imported.'),
-      ));
-    }
-  }
-
-  Future<void> onImportProductData(BuildContext context) async {
-    final filePath = await pickFilePath();
-
-    if (filePath.isPresent) {
-      final file = File(filePath.value);
-      final contents = await file.readAsString();
-
-      await LegacyCsvImporter.importProductData(contents, App.repo);
-
-      final view = ref.read(inventoryProvider.notifier);
-      await view.refresh();
-
-      if (!mounted) return;
-      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-        content: Text('Backup imported.'),
-      ));
-    }
-  }
-
-  Future<void> onImportInventoryData(BuildContext context) async {
-    final filePath = await pickFilePath();
-
-    if (filePath.isPresent) {
-      final file = File(filePath.value);
-      final contents = await file.readAsString();
-
-      await LegacyCsvImporter.importInventoryData(contents, App.repo);
-
-      final view = ref.read(inventoryProvider.notifier);
-      final imageCache = ref.read(itemThumbnailCache.notifier);
-
-      await view.refresh();
-      await view.downloadImages(imageCache);
-
-      if (!mounted) return;
-      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-        content: Text('Backup imported.'),
-      ));
-    }
-  }
-
-  Future<void> onExportInfoBackupPressed(BuildContext context) async {
-    // Generate CSV data
-    String csvData = await LegacyCsvExporter.exportProductData(App.repo);
-
-    // Get application directory
-    Directory appDocDir = await getApplicationDocumentsDirectory();
-
-    // Create export directory if it doesn't exist
-    String exportDir = '${appDocDir.path}/export/';
-    Directory(exportDir).createSync(recursive: true);
-    final dateTime = DateTime.now().toIso8601String().replaceAll('.', '-').replaceAll(':', '-');
-
-    // Write CSV data to a file
-    String filePath = '${appDocDir.path}/info_backup_$dateTime.csv';
-    File file = File(filePath);
-    await file.writeAsString(csvData);
-
-    // Share the file
-    await Share.shareXFiles([XFile(filePath)], text: 'Backup Info');
-
-    if (!mounted) return;
-    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-      content: Text('Backup written to downloads folder.'),
-    ));
-  }
-
-  Future<void> onExportInventoryBackupPressed(BuildContext context) async {
-    // Generate CSV data
-    String csvData = await LegacyCsvExporter.exportInventoryData(App.repo);
-
-    // Get application directory
-    Directory appDocDir = await getApplicationDocumentsDirectory();
-    String exportDir = '${appDocDir.path}/export/';
-
-    // Create export directory if it doesn't exist
-    Directory(exportDir).createSync(recursive: true);
-    final dateTime = DateTime.now().toIso8601String().replaceAll('.', '-').replaceAll(':', '-');
-
-    String filePath = '${appDocDir.path}/inventory_backup_$dateTime.csv';
-
-    // Write CSV data to a file
-    File file = File(filePath);
-    await file.writeAsString(csvData);
-
-    // Share the file
-    await Share.shareXFiles([XFile(filePath)], text: 'Backup Inventory');
-    if (!mounted) return;
-    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-      content: Text('Backup written to downloads folder.'),
-    ));
-  }
-
-  Future<void> onExportHistoryButtonPressed(BuildContext context) async {
-    // Generate CSV data
-    String csvData = await LegacyCsvExporter.exportHistory(App.repo);
-
-    // Get application directory
-    Directory appDocDir = await getApplicationDocumentsDirectory();
-    String exportDir = '${appDocDir.path}/export/';
-
-    // Create export directory if it doesn't exist
-    Directory(exportDir).createSync(recursive: true);
-    final dateTime = DateTime.now().toIso8601String().replaceAll('.', '-').replaceAll(':', '-');
-
-    String filePath = '${appDocDir.path}/history_backup_$dateTime.csv';
-
-    // Write CSV data to a file
-    File file = File(filePath);
-    await file.writeAsString(csvData);
-
-    // Share the file
-    await Share.shareXFiles([XFile(filePath)], text: 'Backup History');
-    if (!mounted) return;
-    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-      content: Text('History backup exported.'),
-    ));
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -231,15 +87,6 @@ class _SettingsPageState extends ConsumerState<SettingsPage> {
                 SettingsTile(
                     title: const Text('Export Backup (Zipped CSV Archive)'),
                     onPressed: onExportButtonPressed),
-                SettingsTile(
-                    title: const Text('Export History Backup (CSV)'),
-                    onPressed: onExportHistoryButtonPressed),
-                SettingsTile(
-                    title: const Text('Export Info Backup (CSV)'),
-                    onPressed: onExportInfoBackupPressed),
-                SettingsTile(
-                    title: const Text('Export Inventory Backup (CSV)'),
-                    onPressed: onExportInventoryBackupPressed),
               ],
             ),
             SettingsSection(
@@ -248,16 +95,6 @@ class _SettingsPageState extends ConsumerState<SettingsPage> {
                 SettingsTile(
                     title: const Text('Import Backup (Zipped CSV Archive)'),
                     onPressed: onImportButtonPressed),
-                SettingsTile(
-                    title: const Text('Import History Info Backup (CSV)'),
-                    onPressed: onImportHistory),
-                SettingsTile(
-                    title: const Text('Import Product Info Backup (CSV)'),
-                    onPressed: onImportProductData),
-                SettingsTile(
-                  title: const Text('Import Inventory Backup (CSV)'),
-                  onPressed: onImportInventoryData,
-                ),
               ],
             ),
           ]),
