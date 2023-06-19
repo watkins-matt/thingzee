c4acea7e1cacf9b244f745a4c231a56ff53cf164
Matt Watkins <48530805+watkins-matt@users.noreply.github.com>
2023-06-18 22:36:44 -0700

    Add test case

---
+++ packages/repository/test/normalized_regressor.dart
@@ -0,0 +1,28 @@
+import 'package:repository/ml/normalizer_map.dart';
+import 'package:repository/ml/regressor.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Normalized Regressor test:', () {
+    test('Values should be as expected.', () async {
+      final Map<int, double> points = {
+        1686280709794: 2.5,
+        1686389999245: 2.2,
+        1686525359796: 2.1,
+        1686631400447: 2.0,
+        1686791671192: 1.3,
+        1686929156394: 0.75,
+      };
+
+      MapNormalizer normalizer = MapNormalizer(points);
+      final weighted = WeightedLeastSquaresLinearRegressor(points);
+      const base = 1687067165106;
+
+      // final regressor = NormalizedRegressor(normalizer, weighted);
+      final weightedSecond = WeightedLeastSquaresLinearRegressor(normalizer.dataPoints);
+
+      expect(weighted.predict(1686280709794), closeTo(2.5, 0.1));
+      expect(weightedSecond.predict(0), closeTo(1, 0.1));
+    });
+  });
+}
