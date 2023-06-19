841942a2cb091470c74eb98e4a3b02408398e5b1
Matt Watkins <48530805+watkins-matt@users.noreply.github.com>
2023-06-18 19:17:41 -0700

    completeTrip

--- lib/pages/shopping/state/shopping_cart.dart
+++ lib/pages/shopping/state/shopping_cart.dart
@@ -1,4 +1,5 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:quiver/core.dart';
 import 'package:repository/database/joined_item_database.dart';
 import 'package:repository/repository.dart';
 import 'package:thingzee/app.dart';
@@ -31,27 +32,30 @@ class ShoppingCart extends StateNotifier<ShoppingCartState> {
   }
 
   void completeTrip() {
-    // TODO: Implement complete code
-    //   for (final item in state.items) {
-    //     final latestProduct = repo.p.get(item.upc); // Get the most recently updated item
-    //     var newProduct = latestProduct.isPresent ? latestProduct.value : item;
-    //     newProduct.lastUpdate = Optional.of(DateTime.now());
+    for (final item in state.items) {
+      // Pull the latest version from the database
+      var latestInventory = repo.inv.get(item.inventory.upc);
+      var inventory = latestInventory.orNull ?? item.inventory;
 
-    //     // First update to the predicted amount so there is not confusion.
-    //     if (newProduct.canPredictAmount) {
-    //       newProduct.amount = newProduct.predictedAmount.roundToDouble();
-    //     }
+      // User might not have updated the amount in a while.
+      // Update the amount to the predicted amount before we increment
+      // it. Still not totally accurate, but should be better
+      // than using a old likely inaccurate amount.
+      if (inventory.canPredict) {
+        inventory.amount = inventory.predictedAmount.roundToDouble();
+      }
 
-    //     newProduct.amount++;
+      final now = DateTime.now();
+      inventory.amount += 1;
+      inventory.history.add(now.millisecondsSinceEpoch, inventory.amount, 2);
+      inventory.lastUpdate = Optional.of(now);
 
-    //     // Update the database with the new amount
-    //     repo.p.put(newProduct);
-    //   }
+      repo.inv.put(inventory);
+    }
 
-    //   state = state.copyWith(
-    //     items: [],
-    //   );
-    // }
+    state = state.copyWith(
+      items: [],
+    );
   }
 }
 
