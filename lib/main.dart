d83f2f782b2a2a11a3a0422471d262cbbd7e862e
Matt Watkins <48530805+watkins-matt@users.noreply.github.com>
2023-06-18 19:17:58 -0700

    Session handling code

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:repository/repository.dart';
 import 'package:repository_ob/repository.dart';
 import 'package:thingzee/app.dart';
+
+final repositoryProvider = Provider<Repository>((ref) {
+  return App.repo;
+});
 
 Future<void> main() async {
   // This line must be first
---
+++ lib/pages/login/state/user_session.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/widgets.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:repository/repository.dart';
+import 'package:thingzee/app.dart';
+
+class UserSession extends StateNotifier<SessionState> {
+  final Repository _repo;
+
+  UserSession(this._repo) : super(SessionState.initial());
+
+  Future<void> login(String email, String password) async {
+    if (!_repo.isMultiUser || _repo is! SharedRepository) {
+      debugPrint('Login not supported for this repository.');
+    }
+
+    final repo = _repo as SharedRepository;
+
+    try {
+      state = SessionState.loading();
+      await repo.loginUser(email, password);
+      state = SessionState.authenticated();
+    } catch (e) {
+      state = SessionState.error(e.toString());
+    }
+  }
+
+  Future<void> register(String username, String email, String password) async {
+    if (!_repo.isMultiUser || _repo is! SharedRepository) {
+      debugPrint('Registration not supported for this repository.');
+    }
+
+    final repo = _repo as SharedRepository;
+
+    try {
+      state = SessionState.loading();
+      await repo.registerUser(username, email, password);
+      state = SessionState.authenticated();
+    } catch (e) {
+      state = SessionState.error(e.toString());
+    }
+  }
+
+  Future<void> logout() async {
+    state = SessionState.initial();
+  }
+}
+
+class SessionState {
+  final String? errorMessage;
+  final bool isLoading;
+  final bool isAuthenticated;
+
+  SessionState({
+    this.errorMessage,
+    this.isLoading = false,
+    this.isAuthenticated = false,
+  });
+
+  factory SessionState.initial() => SessionState();
+
+  factory SessionState.loading() => SessionState(isLoading: true);
+
+  factory SessionState.authenticated() => SessionState(isAuthenticated: true);
+
+  factory SessionState.error(String message) => SessionState(errorMessage: message);
+}
+
+final userSessionProvider = StateNotifierProvider<UserSession, SessionState>((ref) {
+  return UserSession(App.repo);
+});
