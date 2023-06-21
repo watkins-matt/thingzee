c78d1ad30cc25a3c5b9cfa30e883cc44423640df
Matt Watkins <48530805+watkins-matt@users.noreply.github.com>
2023-06-21 13:16:43 -0700

    Initial appwrite database code

---
+++ packages/repository_appw/lib/database/inventory_db.dart
@@ -0,0 +1,142 @@
+import 'dart:async';
+
+import 'package:appwrite/appwrite.dart';
+import 'package:appwrite/models.dart';
+import 'package:quiver/core.dart';
+import 'package:repository/database/inventory_database.dart';
+import 'package:repository/model/inventory.dart';
+
+class AppwriteInventoryDatabase extends InventoryDatabase {
+  static const maxRetries = 3;
+  final Databases _database;
+  final String databaseId;
+  final String collectionId;
+  final _taskQueue = <_QueueTask>[];
+  final _inventory = <String, Inventory>{};
+
+  AppwriteInventoryDatabase(
+    this._database,
+    this.databaseId,
+    this.collectionId,
+  );
+
+  @override
+  List<Inventory> all() => _inventory.values.toList();
+
+  @override
+  void delete(Inventory inv) {
+    _inventory.remove(inv.upc);
+    queueTask(() async {
+      await _database.deleteDocument(
+        databaseId: databaseId,
+        collectionId: collectionId,
+        documentId: inv.upc,
+      );
+    });
+  }
+
+  @override
+  void deleteAll() {
+    for (final inv in _inventory.values.toList()) {
+      delete(inv);
+    }
+    _inventory.clear();
+  }
+
+  @override
+  Optional<Inventory> get(String upc) => Optional.fromNullable(_inventory[upc]);
+
+  @override
+  List<Inventory> getAll(List<String> upcs) => _inventory.entries
+      .where((entry) => upcs.contains(entry.key))
+      .map((entry) => entry.value)
+      .toList();
+
+  @override
+  Map<String, Inventory> map() => Map.unmodifiable(_inventory);
+
+  @override
+  List<Inventory> outs() => _inventory.values.where((inv) => inv.restock).toList();
+
+  @override
+  void put(Inventory inv) {
+    _inventory[inv.upc] = inv;
+
+    queueTask(() async {
+      final documents = await _database.listDocuments(
+        databaseId: databaseId,
+        collectionId: collectionId,
+        queries: [
+          Query.equal('upc', [inv.upc]),
+        ],
+      );
+
+      if (documents.total > 0) {
+        await _database.updateDocument(
+          databaseId: databaseId,
+          collectionId: collectionId,
+          documentId: inv.upc,
+          data: inv.toJson(),
+        );
+      } else {
+        await _database.createDocument(
+          databaseId: databaseId,
+          collectionId: collectionId,
+          documentId: inv.upc,
+          data: inv.toJson(),
+        );
+      }
+    });
+  }
+
+  void queueTask(Future<void> Function() operation) {
+    _taskQueue.add(_QueueTask(operation));
+    scheduleMicrotask(_processQueue);
+  }
+
+  Future<void> refresh() async {
+    try {
+      DocumentList response =
+          await _database.listDocuments(databaseId: databaseId, collectionId: collectionId);
+      var newItems = _documentsToList(response);
+      _inventory.clear();
+      for (final item in newItems) {
+        _inventory[item.upc] = item;
+      }
+    } on AppwriteException catch (e) {
+      print(e);
+    }
+  }
+
+  List<Inventory> _documentsToList(DocumentList documentList) {
+    return documentList.documents.map((doc) {
+      return Inventory.fromJson(doc.data);
+    }).toList();
+  }
+
+  void _processQueue() {
+    if (_taskQueue.isEmpty) {
+      return;
+    }
+
+    final task = _taskQueue.removeAt(0);
+
+    task.operation().then((_) {
+      task.retryCount = 0;
+      _processQueue();
+    }).catchError((e) {
+      if (task.retryCount < maxRetries) {
+        task.retryCount++;
+        _taskQueue.insert(0, task); // Requeue the task at the front
+      }
+      _processQueue();
+    });
+  }
+}
+
+class _QueueTask {
+  final Future<void> Function() operation;
+  int retryCount = 0;
+
+  _QueueTask(this.operation);
+}
--- packages/repository_appw/lib/database/item_db.dart
+++ packages/repository_appw/lib/database/item_db.dart
@@ -1,77 +1,152 @@
+import 'dart:async';
+
 import 'package:appwrite/appwrite.dart';
 import 'package:appwrite/models.dart';
+import 'package:collection/collection.dart';
 import 'package:quiver/core.dart';
 import 'package:repository/database/item_database.dart';
 import 'package:repository/model/filter.dart';
 import 'package:repository/model/item.dart';
 
 class AppwriteItemDatabase extends ItemDatabase {
+  static const maxRetries = 3;
   final Databases _database;
   final String databaseId;
   final String collectionId;
-  // ignore: prefer_final_fields
-  List<Item> _items = [];
+  final _taskQueue = <_QueueTask>[];
+  final _items = <String, Item>{};
 
   AppwriteItemDatabase(this._database, this.databaseId, this.collectionId) {
     refresh();
+    scheduleMicrotask(_processQueue);
   }
 
-  // ignore: unused_element
-  List<Item> _documentsToList(DocumentList documentList) {
-    throw UnimplementedError();
+  @override
+  List<Item> all() => _items.values.toList();
+
+  @override
+  void delete(Item item) {
+    _items.remove(item.upc);
+    queueTask(() async => await _database.deleteDocument(
+        databaseId: databaseId, collectionId: collectionId, documentId: item.upc));
+  }
+
+  @override
+  void deleteAll() {
+    _items.clear();
+    for (final item in _items.values.toList()) {
+      delete(item);
+    }
+  }
+
+  @override
+  List<Item> filter(Filter filter) {
+    return _items.values
+        .where((item) =>
+            (filter.consumable && item.consumable) || (filter.nonConsumable && !item.consumable))
+        .toList();
+  }
+
+  @override
+  Optional<Item> get(String upc) {
+    return Optional.fromNullable(_items[upc]);
+  }
+
+  @override
+  List<Item> getAll(List<String> upcs) {
+    return upcs.map((upc) => _items[upc]).whereNotNull().toList();
+  }
+
+  @override
+  void put(Item item) {
+    _items[item.upc] = item;
+
+    queueTask(() async {
+      try {
+        final documents = await _database
+            .listDocuments(databaseId: databaseId, collectionId: collectionId, queries: [
+          Query.equal('upc', [item.upc]),
+        ]);
+
+        if (documents.total > 0) {
+          // If document exists, update it
+          await _database.updateDocument(
+              databaseId: databaseId,
+              collectionId: collectionId,
+              documentId: item.upc,
+              data: item.toJson());
+        } else {
+          // If document does not exist, create it
+          await _database.createDocument(
+              databaseId: databaseId,
+              collectionId: collectionId,
+              documentId: item.upc,
+              data: item.toJson());
+        }
+      } catch (e) {
+        print('Failed to put item: $e');
+        // Removing the item from local cache since we
+        // failed to add it to the database
+        _items.remove(item.upc);
+        rethrow;
+      }
+    });
+  }
+
+  void queueTask(Future<void> Function() operation) {
+    _taskQueue.add(_QueueTask(operation));
+    scheduleMicrotask(_processQueue);
   }
 
   Future<void> refresh() async {
     try {
-      // ignore: unused_local_variable
       DocumentList response =
           await _database.listDocuments(databaseId: databaseId, collectionId: collectionId);
+      var newItems = _documentsToList(response);
+      _items.clear();
+      for (final item in newItems) {
+        _items[item.upc] = item;
+      }
     } on AppwriteException catch (e) {
       print(e);
     }
   }
 
   @override
-  List<Item> all() {
-    if (_items.isEmpty) {
-      refresh();
-    }
-
-    return _items;
-  }
-
-  @override
-  void delete(Item item) {
-    throw UnimplementedError();
-  }
-
-  @override
-  void deleteAll() {
-    throw UnimplementedError();
-  }
-
-  @override
-  List<Item> filter(Filter filter) {
-    throw UnimplementedError();
-  }
-
-  @override
-  Optional<Item> get(String upc) {
-    throw UnimplementedError();
-  }
-
-  @override
-  List<Item> getAll(List<String> upcs) {
-    throw UnimplementedError();
-  }
-
-  @override
-  void put(Item item) {
-    throw UnimplementedError();
-  }
-
-  @override
   List<Item> search(String string) {
-    throw UnimplementedError();
+    return _items.values.where((item) => item.name.contains(string)).toList();
   }
+
+  List<Item> _documentsToList(DocumentList documentList) {
+    return documentList.documents.map((doc) {
+      return Item.fromJson(doc.data);
+    }).toList();
+  }
+
+  Future<void> _processQueue() async {
+    while (_taskQueue.isNotEmpty) {
+      _QueueTask task = _taskQueue.first;
+
+      if (task.retries >= maxRetries) {
+        print('Failed to execute task after $maxRetries attempts, removing from queue');
+        _taskQueue.removeAt(0);
+        continue;
+      }
+
+      try {
+        await task.operation();
+        _taskQueue.removeAt(0); // Successfully completed the task, so we remove it.
+      } catch (e) {
+        print('Failed to execute task: $e. Retry attempt ${task.retries + 1}');
+        task.retries += 1;
+      }
+    }
+  }
+}
+
+class _QueueTask {
+  final Future<void> Function() operation;
+  int retries = 0;
+
+  _QueueTask(this.operation);
 }
