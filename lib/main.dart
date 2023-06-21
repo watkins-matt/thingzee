8f922f1736e02b4104196ebeceab0d01313f6fb7
Matt Watkins <48530805+watkins-matt@users.noreply.github.com>
2023-06-21 16:33:49 -0700

    Riverpod stack demangle

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:repository/repository.dart';
 import 'package:repository_ob/repository.dart';
+import 'package:stack_trace/stack_trace.dart';
 import 'package:thingzee/app.dart';
 
 final repositoryProvider = Provider<Repository>((ref) {
@@ -11,6 +12,16 @@ final repositoryProvider = Provider<Repository>((ref) {
 Future<void> main() async {
   // This line must be first
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Set demangleStackTrace to handle Riverpod stack traces
+  FlutterError.demangleStackTrace = (StackTrace stack) {
+    if (stack is Trace) {
+      return stack.vmTrace;
+    } else if (stack is Chain) {
+      return stack.toTrace().vmTrace;
+    }
+    return stack;
+  };
 
   // Choose the backend and initialize the database
   App.repo = await ObjectBoxRepository.create();
