5ac5a1276f6f92edf9474a2f2c7de70721cab90a
Matt Watkins <48530805+watkins-matt@users.noreply.github.com>
2023-06-29 08:44:51 -0700

    Add JoinedInventoryDatabase

---
+++ packages/repository/lib/database/joined_inventory_database.dart
@@ -0,0 +1,76 @@
+import 'package:quiver/core.dart';
+import 'package:repository/database/history_database.dart';
+import 'package:repository/database/inventory_database.dart';
+import 'package:repository/ml/history.dart';
+import 'package:repository/model/inventory.dart';
+
+class JoinedInventoryDatabase extends InventoryDatabase {
+  final InventoryDatabase inventoryDatabase;
+  final HistoryDatabase historyDatabase;
+
+  JoinedInventoryDatabase(this.inventoryDatabase, this.historyDatabase);
+
+  @override
+  List<Inventory> all() {
+    List<Inventory> inventoryList = inventoryDatabase.all();
+    inventoryList = historyDatabase.joinList(inventoryList);
+    return inventoryList;
+  }
+
+  @override
+  void delete(Inventory inv) {
+    inventoryDatabase.delete(inv);
+    // Note that we do not delete this history, the user may need
+    // the history information in the future.
+  }
+
+  @override
+  void deleteAll() {
+    inventoryDatabase.deleteAll();
+  }
+
+  @override
+  Optional<Inventory> get(String upc) {
+    Optional<Inventory> inventory = inventoryDatabase.get(upc);
+
+    if (inventory.isPresent) {
+      Optional<History> history = historyDatabase.get(upc);
+
+      if (history.isPresent) {
+        inventory.value.history = history.value;
+      } else {
+        inventory.value.history = History()..upc = upc;
+      }
+    }
+
+    return inventory;
+  }
+
+  @override
+  List<Inventory> getAll(List<String> upcs) {
+    List<Inventory> inventoryList = inventoryDatabase.getAll(upcs);
+    inventoryList = historyDatabase.joinList(inventoryList);
+    return inventoryList;
+  }
+
+  @override
+  Map<String, Inventory> map() {
+    Map<String, Inventory> inventoryMap = inventoryDatabase.map();
+    inventoryMap = historyDatabase.join(inventoryMap);
+    return inventoryMap;
+  }
+
+  @override
+  List<Inventory> outs() {
+    List<Inventory> inventoryOuts = inventoryDatabase.outs();
+    inventoryOuts = historyDatabase.joinList(inventoryOuts);
+    return inventoryOuts;
+  }
+
+  @override
+  void put(Inventory inv) {
+    assert(inv.upc.isNotEmpty && inv.history.upc == inv.upc);
+    inventoryDatabase.put(inv);
+    historyDatabase.put(inv.history);
+  }
+}
