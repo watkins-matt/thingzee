8f3174fa1345cebaa4f4aab6eda988c788539356
Matt Watkins <48530805+watkins-matt@users.noreply.github.com>
2023-07-02 14:46:42 -0700

    Restore previous sessions from preferences

--- packages/repository_appw/lib/repository.dart
+++ packages/repository_appw/lib/repository.dart
@@ -18,32 +18,28 @@ class AppwriteRepository extends SharedRepository {
     _init();
   }
 
-  void _init() {
-    _client = Client();
-    _client.setEndpoint(appwriteEndpoint).setProject(projectId);
-
-    _account = Account(_client);
-    _databases = Databases(_client);
-
-    prefs = DefaultSharedPreferences();
-    items = AppwriteItemDatabase(_databases, 'test', 'item');
-    inv = AppwriteInventoryDatabase(_databases, 'test', 'inventory');
-    hist = AppwriteHistoryDatabase(_databases, 'test', 'history');
-    ready = true;
-  }
-
   @override
   bool get isMultiUser => true;
 
-  bool sync() {
-    if (!loggedIn) {
-      return false;
-    }
+  @override
+  bool get loggedIn =>
+      _session != null && DateTime.now().isBefore(DateTime.parse(_session!.expire));
 
-    (items as AppwriteItemDatabase).online = true;
-    (inv as AppwriteInventoryDatabase).online = true;
-    (hist as AppwriteHistoryDatabase).online = true;
-    return true;
+  @override
+  Future<void> loginUser(String email, String password) async {
+    try {
+      await _loadSession();
+
+      // If no valid session, then login
+      if (_session == null) {
+        _session = await _account.createEmailSession(email: email, password: password);
+        await prefs.setString('appwrite_session_id', _session!.$id);
+        await prefs.setString('appwrite_session_expire', _session!.expire);
+        sync();
+      }
+    } catch (e) {
+      throw Exception('Failed to login user: $e');
+    }
   }
 
   @override
@@ -62,17 +58,47 @@ class AppwriteRepository extends SharedRepository {
     }
   }
 
-  @override
-  Future<void> loginUser(String email, String password) async {
-    try {
-      _session = await _account.createEmailSession(email: email, password: password);
-      sync();
-    } catch (e) {
-      throw Exception('Failed to login user: $e');
+  bool sync() {
+    if (!loggedIn) {
+      return false;
     }
+
+    (items as AppwriteItemDatabase).online = true;
+    (inv as AppwriteInventoryDatabase).online = true;
+    (hist as AppwriteHistoryDatabase).online = true;
+    return true;
   }
 
-  @override
-  bool get loggedIn =>
-      _session != null && DateTime.now().isBefore(DateTime.parse(_session!.expire));
+  void _init() {
+    _client = Client();
+    _client.setEndpoint(appwriteEndpoint).setProject(projectId);
+
+    _account = Account(_client);
+    _databases = Databases(_client);
+
+    prefs = DefaultSharedPreferences();
+    items = AppwriteItemDatabase(_databases, 'test', 'item');
+    inv = AppwriteInventoryDatabase(_databases, 'test', 'inventory');
+    hist = AppwriteHistoryDatabase(_databases, 'test', 'history');
+    ready = true;
+  }
+
+  Future<void> _loadSession() async {
+    if (prefs.containsKey('appwrite_session_id') && prefs.containsKey('appwrite_session_expire')) {
+      String sessionId = prefs.getString('appwrite_session_id')!;
+      String expiration = prefs.getString('appwrite_session_expire')!;
+      final expireDate = DateTime.parse(expiration);
+
+      if (DateTime.now().isBefore(expireDate)) {
+        _session = await _account.getSession(sessionId: sessionId);
+        sync();
+      }
+
+      // Just remove the preferences if the session is expired
+      else {
+        await prefs.remove('appwrite_session_id');
+        await prefs.remove('appwrite_session_expire');
+      }
+    }
+  }
 }
