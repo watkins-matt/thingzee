ecef3dbf73ee97c46a5393067a01e4ed596f15c1
Matt Watkins <48530805+watkins-matt@users.noreply.github.com>
2023-07-03 16:26:45 -0700

    Add autofill hints

--- lib/pages/login/login_page.dart
+++ lib/pages/login/login_page.dart
@@ -93,144 +93,148 @@ class LoginPage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       backgroundColor: Colors.blue,
-      body: Center(
-        child: Card(
-          color: Colors.white,
-          shadowColor: Colors.black,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(15),
-          ),
-          child: Container(
-            width: 300,
-            padding: const EdgeInsets.all(20),
-            child: SingleChildScrollView(
-              child: Form(
-                key: _formKey,
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    const Text(
-                      'Login',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 36,
-                        fontWeight: FontWeight.w600,
-                        letterSpacing: .5,
-                      ),
-                    ),
-                    const SizedBox(height: 20),
-                    TextFormField(
-                      onChanged: (value) {
-                        ref.read(loginStateProvider.notifier).setEmail(value);
-                      },
-                      validator: (val) {
-                        if (val!.isEmpty) {
-                          return 'Email cannot be empty.';
-                        } else {
-                          return null;
-                        }
-                      },
-                      decoration: InputDecoration(
-                        prefixIcon: const Icon(Icons.email_outlined),
-                        labelText: 'Email',
-                        filled: true,
-                        fillColor: Colors.blue[75],
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(12),
-                          borderSide: BorderSide.none,
+      body: AutofillGroup(
+        child: Center(
+          child: Card(
+            color: Colors.white,
+            shadowColor: Colors.black,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: Container(
+              width: 300,
+              padding: const EdgeInsets.all(20),
+              child: SingleChildScrollView(
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      const Text(
+                        'Login',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 36,
+                          fontWeight: FontWeight.w600,
+                          letterSpacing: .5,
                         ),
                       ),
-                    ),
-                    const SizedBox(height: 20),
-                    TextFormField(
-                      obscureText: true,
-                      onChanged: (value) {
-                        ref.read(loginStateProvider.notifier).setPassword(value);
-                      },
-                      validator: (val) {
-                        if (val!.isEmpty) {
-                          return 'Password cannot be empty.';
-                        } else {
-                          return null;
-                        }
-                      },
-                      decoration: InputDecoration(
-                        prefixIcon: const Icon(Icons.lock_outline),
-                        labelText: 'Password',
-                        filled: true,
-                        fillColor: Colors.blue[75],
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(12),
-                          borderSide: BorderSide.none,
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 20),
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        foregroundColor: Colors.white,
-                        backgroundColor: Colors.blue,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        elevation: 5,
-                        padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
-                      ),
-                      child: ref.watch(loginStateProvider).loading
-                          ? const CircularProgressIndicator(
-                              color: Colors.white,
-                              backgroundColor: Colors.blue,
-                            )
-                          : const Text('Login'),
-                      onPressed: () async {
-                        if (!ref.read(loginStateProvider).loading &&
-                            _formKey.currentState!.validate()) {
-                          bool success = await ref.read(loginStateProvider.notifier).login(ref);
-                          if (success && context.mounted) {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              const SnackBar(
-                                content: Text('Logged in successfully.'),
-                                duration: Duration(seconds: 1),
-                              ),
-                            );
-                            await Navigator.pushReplacement(
-                              context,
-                              MaterialPageRoute(builder: (context) => const HomePage()),
-                            );
+                      const SizedBox(height: 20),
+                      TextFormField(
+                        autofillHints: const [AutofillHints.email],
+                        onChanged: (value) {
+                          ref.read(loginStateProvider.notifier).setEmail(value);
+                        },
+                        validator: (val) {
+                          if (val!.isEmpty) {
+                            return 'Email cannot be empty.';
+                          } else {
+                            return null;
                           }
-                        }
-                      },
-                    ),
-                    const SizedBox(height: 20),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        TextButton(
-                          onPressed: () {
-                            // Navigate to Forgot Password page
-                          },
-                          child: const Text(
-                            'Forgot Password?',
-                            style: TextStyle(
-                              color: Colors.blue,
-                            ),
+                        },
+                        decoration: InputDecoration(
+                          prefixIcon: const Icon(Icons.email_outlined),
+                          labelText: 'Email',
+                          filled: true,
+                          fillColor: Colors.blue[75],
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12),
+                            borderSide: BorderSide.none,
                           ),
                         ),
-                        TextButton(
-                          onPressed: () {
-                            Navigator.pushReplacement(
-                                context, NoAnimationRoute(child: const RegisterPage()));
-                          },
-                          child: const Text(
-                            'Register',
-                            style: TextStyle(
-                              color: Colors.blue,
-                            ),
+                      ),
+                      const SizedBox(height: 20),
+                      TextFormField(
+                        autofillHints: const [AutofillHints.password],
+                        obscureText: true,
+                        onChanged: (value) {
+                          ref.read(loginStateProvider.notifier).setPassword(value);
+                        },
+                        validator: (val) {
+                          if (val!.isEmpty) {
+                            return 'Password cannot be empty.';
+                          } else {
+                            return null;
+                          }
+                        },
+                        decoration: InputDecoration(
+                          prefixIcon: const Icon(Icons.lock_outline),
+                          labelText: 'Password',
+                          filled: true,
+                          fillColor: Colors.blue[75],
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12),
+                            borderSide: BorderSide.none,
                           ),
                         ),
-                      ],
-                    ),
-                  ],
+                      ),
+                      const SizedBox(height: 20),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          foregroundColor: Colors.white,
+                          backgroundColor: Colors.blue,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          elevation: 5,
+                          padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+                        ),
+                        child: ref.watch(loginStateProvider).loading
+                            ? const CircularProgressIndicator(
+                                color: Colors.white,
+                                backgroundColor: Colors.blue,
+                              )
+                            : const Text('Login'),
+                        onPressed: () async {
+                          if (!ref.read(loginStateProvider).loading &&
+                              _formKey.currentState!.validate()) {
+                            bool success = await ref.read(loginStateProvider.notifier).login(ref);
+                            if (success && context.mounted) {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                  content: Text('Logged in successfully.'),
+                                  duration: Duration(seconds: 1),
+                                ),
+                              );
+                              await Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(builder: (context) => const HomePage()),
+                              );
+                            }
+                          }
+                        },
+                      ),
+                      const SizedBox(height: 20),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          TextButton(
+                            onPressed: () {
+                              // Navigate to Forgot Password page
+                            },
+                            child: const Text(
+                              'Forgot Password?',
+                              style: TextStyle(
+                                color: Colors.blue,
+                              ),
+                            ),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              Navigator.pushReplacement(
+                                  context, NoAnimationRoute(child: const RegisterPage()));
+                            },
+                            child: const Text(
+                              'Register',
+                              style: TextStyle(
+                                color: Colors.blue,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
