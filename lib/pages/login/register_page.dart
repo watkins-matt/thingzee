5b8f6e295e0d368ada555747867cae40c0db3960
Matt Watkins <48530805+watkins-matt@users.noreply.github.com>
2023-07-18 15:56:27 -0700

    Separate state

--- lib/pages/login/register_page.dart
+++ lib/pages/login/register_page.dart
@@ -2,12 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:log/log.dart';
 import 'package:thingzee/pages/login/login_page.dart';
-import 'package:thingzee/pages/login/state/user_profile.dart';
-import 'package:thingzee/pages/login/state/user_session.dart';
-
-final registerStateProvider = StateNotifierProvider<RegisterStateNotifier, RegisterState>((ref) {
-  return RegisterStateNotifier();
-});
+import 'package:thingzee/pages/login/state/register_state.dart';
 
 class RegisterPage extends ConsumerStatefulWidget {
   const RegisterPage({Key? key}) : super(key: key);
@@ -20,83 +15,6 @@ class RegisterPage extends ConsumerStatefulWidget {
       context,
       MaterialPageRoute(builder: (context) => const RegisterPage()),
     );
-  }
-}
-
-class RegisterState {
-  String email = '';
-  String username = '';
-  String password = '';
-  String confirmPassword = '';
-  String errorText = '';
-
-  RegisterState(
-      {this.email = '',
-      this.username = '',
-      this.password = '',
-      this.confirmPassword = '',
-      this.errorText = ''});
-}
-
-class RegisterStateNotifier extends StateNotifier<RegisterState> {
-  RegisterStateNotifier() : super(RegisterState());
-
-  bool get passwordsMatch => state.password == state.confirmPassword;
-
-  Future<void> register(WidgetRef ref) async {
-    final userProfile = ref.read(userProfileProvider.notifier);
-    final userSession = ref.read(userSessionProvider.notifier);
-
-    try {
-      await userSession.register(state.username, state.email, state.password);
-    } catch (e) {
-      state = RegisterState(
-          email: state.email,
-          username: state.username,
-          password: state.password,
-          confirmPassword: state.confirmPassword,
-          errorText: e.toString());
-      return;
-    }
-
-    if (ref.read(userSessionProvider).isAuthenticated) {
-      userProfile.email = state.email;
-    }
-  }
-
-  void setConfirmPassword(String value) {
-    state = RegisterState(
-        email: state.email,
-        username: state.username,
-        password: state.password,
-        confirmPassword: value,
-        errorText: state.errorText);
-  }
-
-  void setEmail(String value) {
-    state = RegisterState(
-        email: value,
-        username: state.username,
-        password: state.password,
-        confirmPassword: state.confirmPassword,
-        errorText: state.errorText);
-  }
-
-  void setPassword(String value) {
-    state = RegisterState(
-        email: state.email,
-        username: state.username,
-        password: value,
-        confirmPassword: state.confirmPassword,
-        errorText: state.errorText);
-  }
-
-  void setUsername(String value) {
-    state = RegisterState(
-        email: state.email,
-        username: value,
-        password: state.password,
-        confirmPassword: state.confirmPassword);
   }
 }
 
---
+++ lib/pages/login/state/register_state.dart
@@ -0,0 +1,84 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:thingzee/pages/login/state/user_profile.dart';
+import 'package:thingzee/pages/login/state/user_session.dart';
+
+final registerStateProvider = StateNotifierProvider<RegisterStateNotifier, RegisterState>((ref) {
+  return RegisterStateNotifier();
+});
+
+class RegisterState {
+  String email = '';
+  String username = '';
+  String password = '';
+  String confirmPassword = '';
+  String errorText = '';
+
+  RegisterState(
+      {this.email = '',
+      this.username = '',
+      this.password = '',
+      this.confirmPassword = '',
+      this.errorText = ''});
+}
+
+class RegisterStateNotifier extends StateNotifier<RegisterState> {
+  RegisterStateNotifier() : super(RegisterState());
+
+  bool get passwordsMatch => state.password == state.confirmPassword;
+
+  Future<void> register(WidgetRef ref) async {
+    final userProfile = ref.read(userProfileProvider.notifier);
+    final userSession = ref.read(userSessionProvider.notifier);
+
+    try {
+      await userSession.register(state.username, state.email, state.password);
+    } catch (e) {
+      state = RegisterState(
+          email: state.email,
+          username: state.username,
+          password: state.password,
+          confirmPassword: state.confirmPassword,
+          errorText: e.toString());
+      return;
+    }
+
+    if (ref.read(userSessionProvider).isAuthenticated) {
+      userProfile.email = state.email;
+    }
+  }
+
+  void setConfirmPassword(String value) {
+    state = RegisterState(
+        email: state.email,
+        username: state.username,
+        password: state.password,
+        confirmPassword: value,
+        errorText: state.errorText);
+  }
+
+  void setEmail(String value) {
+    state = RegisterState(
+        email: value,
+        username: state.username,
+        password: state.password,
+        confirmPassword: state.confirmPassword,
+        errorText: state.errorText);
+  }
+
+  void setPassword(String value) {
+    state = RegisterState(
+        email: state.email,
+        username: state.username,
+        password: value,
+        confirmPassword: state.confirmPassword,
+        errorText: state.errorText);
+  }
+
+  void setUsername(String value) {
+    state = RegisterState(
+        email: state.email,
+        username: value,
+        password: state.password,
+        confirmPassword: state.confirmPassword);
+  }
+}
