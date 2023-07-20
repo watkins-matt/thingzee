3e61794b6c49f485845d81bee4e112c9c126fe12
Matt Watkins <48530805+watkins-matt@users.noreply.github.com>
2023-07-19 18:40:48 -0700

    Verification dialog

---
+++ lib/pages/login/widget/verify_account_dialog.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class VerifyAccountDialog extends StatelessWidget {
+  final VoidCallback? onResendEmailPressed;
+  const VerifyAccountDialog({
+    super.key,
+    this.onResendEmailPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Text('Account Verification Required'),
+      content: const Text('You need to verify your email address before you can use '
+          'online features. Please click the link sent to your '
+          'email in order to verify your address.'),
+      actions: [
+        TextButton(
+          child: const Text('Resend Verification Email'),
+          onPressed: () {
+            if (onResendEmailPressed != null) {
+              onResendEmailPressed!();
+            }
+
+            Navigator.of(context).pop();
+          },
+        ),
+        TextButton(
+          child: const Text('OK'),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+      ],
+    );
+  }
+
+  static Future<bool> show(BuildContext context) async {
+    final result =
+        await showDialog<bool>(context: context, builder: (context) => const VerifyAccountDialog());
+    return result ?? false;
+  }
+}
