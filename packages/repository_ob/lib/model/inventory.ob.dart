f250a9aae497b483bfab44a8de1ea89e7e90f515
Matt Watkins <48530805+watkins-matt@users.noreply.github.com>
2023-08-09 14:19:50 -0700

    Missing import

--- packages/repository_ob/lib/model/inventory.ob.dart
+++ packages/repository_ob/lib/model/inventory.ob.dart
@@ -3,6 +3,8 @@ import 'dart:core';
 import 'package:objectbox/objectbox.dart';
 import 'package:repository/ml/history.dart';
 import 'package:repository/model/inventory.dart';
+import 'package:repository/model/serializer_datetime.dart';
+
 @Entity()
 class ObjectBoxInventory {
   late double amount;
@@ -32,9 +34,38 @@ class ObjectBoxInventory {
     iuid = original.iuid;
     units = original.units;
   }
+  List<String> get dbExpirationDates {
+    List<String> dates = [];
+    for (final exp in expirationDates) {
+      dates.add(exp.millisecondsSinceEpoch.toString());
+    }
+
+    return dates;
+  }
+
+  set dbExpirationDates(List<String> dates) {
+    expirationDates.clear();
+
+    for (final date in dates) {
+      int? timestamp = int.tryParse(date);
+
+      if (timestamp != null) {
+        expirationDates.add(DateTime.fromMillisecondsSinceEpoch(timestamp));
+      }
+    }
+  }
+
+  int get dbLastUpdate {
+    return lastUpdate != null ? lastUpdate!.millisecondsSinceEpoch : 0;
+  }
+
+  set dbLastUpdate(int value) {
+    lastUpdate = value != 0 ? DateTime.fromMillisecondsSinceEpoch(value) : null;
+  }
+
   Inventory toInventory() {
-      // Ensure history is in a consistent state
-      history.upc = upc;
+    // Ensure history is in a consistent state
+    history.upc = upc;
     return Inventory()
       ..amount = amount
       ..unitCount = unitCount
@@ -45,41 +76,10 @@ class ObjectBoxInventory {
       ..restock = restock
       ..upc = upc
       ..iuid = iuid
-      ..units = units
-    ;
-  }
-int get dbLastUpdate {
-  return lastUpdate != null ? lastUpdate!.millisecondsSinceEpoch : 0;
-}
-
-set dbLastUpdate(int value) {
-  lastUpdate = value != 0
-      ? DateTime.fromMillisecondsSinceEpoch(value)
-      : null;
-}
-
-List<String> get dbExpirationDates {
-  List<String> dates = [];
-  for (final exp in expirationDates) {
-    dates.add(exp.millisecondsSinceEpoch.toString());
-  }
-
-  return dates;
-}
-
-set dbExpirationDates(List<String> dates) {
-  expirationDates.clear();
-
-  for (final date in dates) {
-    int? timestamp = int.tryParse(date);
-
-    if (timestamp != null) {
-      expirationDates.add(DateTime.fromMillisecondsSinceEpoch(timestamp));
-    }
+      ..units = units;
   }
 }
 
-}
 @Entity()
 class ObjectBoxNullableDateTimeSerializer {
   @Id()
@@ -87,7 +87,6 @@ class ObjectBoxNullableDateTimeSerializer {
   ObjectBoxNullableDateTimeSerializer();
   ObjectBoxNullableDateTimeSerializer.from();
   NullableDateTimeSerializer toNullableDateTimeSerializer() {
-    return NullableDateTimeSerializer(
-    );
+    return NullableDateTimeSerializer();
   }
 }
