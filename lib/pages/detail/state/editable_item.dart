7aa17ac8abed2959268ae8612c10a83a1b6e6fc3
Matt Watkins <48530805+watkins-matt@users.noreply.github.com>
2023-08-24 16:31:23 -0700

    Clarify comment

--- lib/pages/detail/state/editable_item.dart
+++ lib/pages/detail/state/editable_item.dart
@@ -197,7 +197,8 @@ class EditableItem extends StateNotifier<EditableItemState> {
     // Make sure we update the last updated time
     state.inventory.lastUpdate = saveTimestamp;
 
-    // Save the inventory
+    // Save the inventory. Note that we use a joined db
+    // here so saving the inventory also saves the history
     assert(state.inventory.upc == state.item.upc);
     repo.inv.put(state.inventory);
 
