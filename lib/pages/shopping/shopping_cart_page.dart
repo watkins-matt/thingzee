c326964563bdaf48ac69cbca643ad891b6d70401
Matt Watkins <48530805+watkins-matt@users.noreply.github.com>
2023-09-20 16:10:27 -0700

    Refactor, trip completed changes to inventory view

--- lib/pages/shopping/shopping_cart_page.dart
+++ lib/pages/shopping/shopping_cart_page.dart
@@ -1,18 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:thingzee/pages/bottom_nav_bar/state/bottom_nav_state.dart';
+import 'package:thingzee/pages/inventory/state/inventory_view.dart';
 import 'package:thingzee/pages/shopping/confirmation_dialog.dart';
 import 'package:thingzee/pages/shopping/state/shopping_cart.dart';
+import 'package:thingzee/pages/shopping/state/shopping_list.dart';
 import 'package:thingzee/pages/shopping/widget/shopping_cart_list_tile.dart';
 
 class ShoppingCartPage extends ConsumerWidget {
   const ShoppingCartPage({Key? key}) : super(key: key);
-
-  static Future<void> push(BuildContext context) async {
-    await Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => const ShoppingCartPage()),
-    );
-  }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -20,12 +16,7 @@ class ShoppingCartPage extends ConsumerWidget {
       appBar: AppBar(
         actions: [
           TextButton.icon(
-              onPressed: () async {
-                if (await TripCompletedConfirmationDialog.show(context)) {
-                  ref.read(shoppingCartProvider.notifier).completeTrip();
-                  // ref.read(shoppingListProvider.notifier).refresh();
-                }
-              },
+              onPressed: () => _handleTripCompleted(context, ref),
               icon: const Icon(Icons.check),
               label: const Text('Done'))
         ],
@@ -34,16 +25,51 @@ class ShoppingCartPage extends ConsumerWidget {
         children: [
           Expanded(
             child: ListView.separated(
-              itemBuilder: (context, index) {
-                final item = ref.watch(shoppingCartProvider).items[index];
-                return ShoppingCartListTile(joinedItem: item, index: index);
-              },
-              separatorBuilder: (context, index) => const Divider(height: 1, color: Colors.grey),
-              itemCount: ref.watch(shoppingCartProvider).items.length,
+              itemBuilder: (context, index) => _buildShoppingCartItem(context, index, ref),
+              separatorBuilder: (context, index) => _buildShoppingCartSeparator(),
+              itemCount: _getShoppingCartItemCount(ref),
             ),
           ),
         ],
       ),
+    );
+  }
+
+  Widget _buildShoppingCartItem(BuildContext context, int index, WidgetRef ref) {
+    final item = ref.watch(shoppingCartProvider).items[index];
+    return ShoppingCartListTile(joinedItem: item, index: index);
+  }
+
+  Widget _buildShoppingCartSeparator() {
+    return const Divider(height: 1, color: Colors.grey);
+  }
+
+  int _getShoppingCartItemCount(WidgetRef ref) {
+    return ref.watch(shoppingCartProvider).items.length;
+  }
+
+  Future<void> _handleTripCompleted(BuildContext context, WidgetRef ref) async {
+    // Show confirmation dialog
+    if (await TripCompletedConfirmationDialog.show(context)) {
+      // Complete trip will update inventory information
+      ref.read(shoppingCartProvider.notifier).completeTrip();
+
+      // Refresh the list and inventory view
+      ref.read(shoppingListProvider.notifier).refresh();
+      await ref.read(inventoryProvider.notifier).refresh();
+
+      // Switch back to the inventory view tab
+      ref.read(bottomNavBarIndexProvider.notifier).state = 0;
+
+      // Hide the shopping cart page
+      if (context.mounted) Navigator.pop(context);
+    }
+  }
+
+  static Future<void> push(BuildContext context) async {
+    await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const ShoppingCartPage()),
     );
   }
 }
