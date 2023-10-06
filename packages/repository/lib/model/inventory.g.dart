540325af3af21bed1fd564c577ce4198b940a6fc
Matt Watkins <48530805+watkins-matt@users.noreply.github.com>
2023-10-06 10:37:17 -0700

    Default json values

--- packages/repository/lib/model/inventory.g.dart
+++ packages/repository/lib/model/inventory.g.dart
@@ -7,18 +7,20 @@ part of 'inventory.dart';
 // **************************************************************************
 
 Inventory _$InventoryFromJson(Map<String, dynamic> json) => Inventory()
-  ..amount = (json['amount'] as num).toDouble()
-  ..unitCount = json['unitCount'] as int
+  ..amount = (json['amount'] as num?)?.toDouble() ?? 0
+  ..unitCount = json['unitCount'] as int? ?? 1
   ..lastUpdate = _$JsonConverterFromJson<int, DateTime?>(
       json['lastUpdate'], const NullableDateTimeSerializer().fromJson)
-  ..expirationDates = (json['expirationDates'] as List<dynamic>)
-      .map((e) => DateTime.parse(e as String))
-      .toList()
+  ..expirationDates = (json['expirationDates'] as List<dynamic>?)
+          ?.map((e) => DateTime.parse(e as String))
+          .toList() ??
+      []
   ..locations =
-      (json['locations'] as List<dynamic>).map((e) => e as String).toList()
-  ..restock = json['restock'] as bool
-  ..upc = json['upc'] as String
-  ..iuid = json['iuid'] as String;
+      (json['locations'] as List<dynamic>?)?.map((e) => e as String).toList() ??
+          []
+  ..restock = json['restock'] as bool? ?? true
+  ..upc = json['upc'] as String? ?? ''
+  ..iuid = json['iuid'] as String? ?? '';
 
 Map<String, dynamic> _$InventoryToJson(Inventory instance) => <String, dynamic>{
       'amount': instance.amount,
