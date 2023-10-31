6dd4fccb22806d412e3c608716f280180491e1ee
Matt Watkins <48530805+watkins-matt@users.noreply.github.com>
2023-10-31 13:11:13 -0700

    Uid naming refactor

--- packages/repository/lib/model/identifier.g.dart
+++ packages/repository/lib/model/identifier.g.dart
@@ -8,11 +8,11 @@ part of 'identifier.dart';
 
 ItemIdentifier _$ItemIdentifierFromJson(Map<String, dynamic> json) => ItemIdentifier()
   ..type = const IdentifierTypeSerializer().fromJson(json['type'] as String)
-  ..uid = json['iuid'] as String
+  ..uid = json['uid'] as String
   ..value = json['value'] as String;
 
 Map<String, dynamic> _$ItemIdentifierToJson(ItemIdentifier instance) => <String, dynamic>{
       'type': const IdentifierTypeSerializer().toJson(instance.type),
-      'iuid': instance.uid,
+      'uid': instance.uid,
       'value': instance.value,
     };
