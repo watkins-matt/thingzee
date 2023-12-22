4cbe82b7d4f9bf2bc4bd035d6177dddfc536a837
Matt Watkins <48530805+watkins-matt@users.noreply.github.com>
2023-12-21 17:17:09 -0800

    Initial identifier database

---
+++ packages/repository/lib/database/identifier_database.dart
@@ -0,0 +1,12 @@
+import 'package:repository/model/identifier.dart';
+
+abstract class IdentifierDatabase {
+  List<ItemIdentifier> all();
+  void delete(ItemIdentifier inv);
+  void deleteAll();
+  String get(String identifier);
+  List<ItemIdentifier> getAll(List<String> identifiers);
+  List<ItemIdentifier> getChanges(DateTime since);
+  Map<String, ItemIdentifier> map();
+  void put(ItemIdentifier identifier);
+}
