f02381efb00cb0732f5a276967a71e0d69183f58
Matt Watkins <48530805+watkins-matt@users.noreply.github.com>
2023-12-21 16:27:34 -0800

    Clean up receipt edit item dialog

--- lib/pages/receipt_scanner/edit_item_dialog.dart
+++ lib/pages/receipt_scanner/edit_item_dialog.dart
@@ -22,43 +22,73 @@ class _EditItemDialogState extends State<EditItemDialog> {
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-      title: const Text('Edit Item'),
-      content: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Row(
-            children: [
-              Expanded(child: _buildTextField(nameController, 'Name')),
-              const SizedBox(width: 8),
-              Expanded(child: _buildTextField(barcodeController, 'Barcode')),
-            ],
-          ),
-          Row(
-            children: [
-              Expanded(child: _buildTextField(quantityController, 'Quantity')),
-              const SizedBox(width: 8),
-              Expanded(child: _buildTextField(priceController, 'Price')),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              SwitchListTile(
-                title: const Text('Taxable'),
-                value: taxable,
-                onChanged: (bool value) {
-                  setState(() {
-                    taxable = value;
-                  });
-                },
+      content: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(4),
+              child: Row(
+                children: [
+                  Expanded(child: _buildTextField(nameController, 'Name')),
+                  const SizedBox(width: 8),
+                  Expanded(
+                      child: _buildTextField(barcodeController, 'Barcode',
+                          keyboardType: TextInputType.number)),
+                ],
               ),
-              ElevatedButton(
-                onPressed: () => _changeBottleDeposit(context),
-                child: const Text('Change Bottle Deposit'),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(4),
+              child: Row(
+                children: [
+                  Expanded(
+                      child: _buildTextField(quantityController, 'Quantity',
+                          selectAllOnTap: true, keyboardType: TextInputType.number)),
+                  const SizedBox(width: 8),
+                  Expanded(
+                      child: _buildTextField(priceController, 'Price',
+                          selectAllOnTap: false, keyboardType: TextInputType.number)),
+                ],
               ),
-            ],
-          ),
-        ],
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 16),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          taxable = !taxable;
+                        });
+                      },
+                      style: ElevatedButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(horizontal: 8),
+                        textStyle: const TextStyle(fontSize: 12),
+                      ),
+                      child: Text(taxable ? 'Taxable' : 'Not Taxable',
+                          style: const TextStyle(fontSize: 12)),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 16),
+                    child: ElevatedButton(
+                      onPressed: () => _changeBottleDeposit(context),
+                      style: ElevatedButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(horizontal: 8),
+                        textStyle: const TextStyle(fontSize: 12),
+                      ),
+                      child: const Text('Bottle Deposit', style: TextStyle(fontSize: 12)),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
       actions: <Widget>[
         TextButton(
@@ -96,10 +126,16 @@ class _EditItemDialogState extends State<EditItemDialog> {
     bottleDeposit = widget.item.bottleDeposit;
   }
 
-  Widget _buildTextField(TextEditingController controller, String label) {
+  Widget _buildTextField(TextEditingController controller, String label,
+      {bool selectAllOnTap = false, TextInputType keyboardType = TextInputType.text}) {
     return TextField(
       controller: controller,
       decoration: InputDecoration(labelText: label),
+      keyboardType: keyboardType,
+      onTap: selectAllOnTap
+          ? () => controller.selection =
+              TextSelection(baseOffset: 0, extentOffset: controller.text.length)
+          : null,
     );
   }
 
