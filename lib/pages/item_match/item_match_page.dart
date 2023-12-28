d9197fe2ccd2f7ebddba0245bf0c90d27705f488
Matt Watkins <48530805+watkins-matt@users.noreply.github.com>
2023-12-27 16:45:16 -0800

    Clean up item match page

--- lib/pages/item_match/item_match_page.dart
+++ lib/pages/item_match/item_match_page.dart
@@ -26,6 +26,7 @@ class ItemMatchPage extends ConsumerStatefulWidget {
 
 class _ItemMatchPageState extends ConsumerState<ItemMatchPage> {
   String searchQuery = '';
+  late TextEditingController _controller;
 
   @override
   Widget build(BuildContext context) {
@@ -34,27 +35,18 @@ class _ItemMatchPageState extends ConsumerState<ItemMatchPage> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Match Item: ${widget.receiptItem.name}'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.camera_alt),
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: const Icon(Icons.add),
-            onPressed: () {},
-          ),
-        ],
       ),
       body: Column(
         children: [
           Padding(
             padding: const EdgeInsets.all(8),
             child: TextField(
+              controller: _controller,
               onChanged: (value) {
                 setState(() {
                   searchQuery = value;
                 });
-                ref.read(inventoryProvider.notifier).search(value);
+                ref.read(inventoryProvider.notifier).fuzzySearch(value);
               },
               decoration: const InputDecoration(
                 labelText: 'Search',
@@ -71,9 +63,10 @@ class _ItemMatchPageState extends ConsumerState<ItemMatchPage> {
                 return ListTile(
                   title: Text(item.item.name),
                   subtitle: Text(item.item.type),
-                  trailing: Text('Qty: ${item.inventory.amount}'),
                   onTap: () {
-                    // Handle item selection
+                    // Handle item selection by popping the current page
+                    // and returning the selected item
+                    Navigator.of(context).pop(item);
                   },
                 );
               },
@@ -82,5 +75,31 @@ class _ItemMatchPageState extends ConsumerState<ItemMatchPage> {
         ],
       ),
     );
+  }
+
+  void initializeSearchQuery() {
+    // Extract the first word from the receipt item's name that is >= 3 characters.
+    String name = widget.receiptItem.name;
+    // Find the first word in the name, ignoring leading punctuation and considering only letters.
+    RegExp regex = RegExp(r'\b[a-zA-Z]{3,}\b');
+    Match? match = regex.firstMatch(name);
+
+    if (match != null) {
+      // Set the found word as the initial search query
+      searchQuery = name.substring(match.start, match.end);
+    } else {
+      // Default to an empty string if no matching word is found
+      searchQuery = '';
+    }
+
+    // Update the inventory provider with the initial search query.
+    ref.read(inventoryProvider.notifier).fuzzySearch(searchQuery);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initializeSearchQuery();
+    _controller = TextEditingController(text: searchQuery);
   }
 }
