2226c207576db27aea96852ba3fdd5f46d9e9166
Matt Watkins <48530805+watkins-matt@users.noreply.github.com>
2024-01-02 17:20:23 -0800

    Add created/updated

--- packages/repository/lib/model/identifier.dart
+++ packages/repository/lib/model/identifier.dart
@@ -1,5 +1,6 @@
 import 'package:json_annotation/json_annotation.dart';
 import 'package:meta/meta.dart';
+import 'package:repository/model/serializer_datetime.dart';
 
 part 'identifier.g.dart';
 
@@ -15,11 +16,25 @@ class ItemIdentifier {
   @JsonKey(defaultValue: '')
   final String uid; // The global uid of the item that this identifier maps to
 
-  const ItemIdentifier({
+  @NullableDateTimeSerializer()
+  final DateTime? created;
+
+  @NullableDateTimeSerializer()
+  final DateTime? updated;
+
+  ItemIdentifier({
     this.type = '',
     this.value = '',
     this.uid = '',
-  });
+    DateTime? created,
+    DateTime? updated,
+  })  :
+        // Initialize 'created' and 'updated' date-times.
+        // If 'created' is not provided, it defaults to the value of 'updated' if that was provided,
+        // otherwise to the current time. If 'updated' is not provided, it defaults to the value of 'created',
+        // ensuring both fields are synchronized and non-null. If both are provided, their values are retained.
+        created = created ?? _defaultDateTime(updated),
+        updated = updated ?? _defaultDateTime(created);
 
   factory ItemIdentifier.fromJson(Map<String, dynamic> json) => _$ItemIdentifierFromJson(json);
 
@@ -27,13 +42,22 @@ class ItemIdentifier {
     String? type,
     String? value,
     String? uid,
+    DateTime? created,
+    DateTime? updated,
   }) {
     return ItemIdentifier(
       type: type ?? this.type,
       value: value ?? this.value,
       uid: uid ?? this.uid,
+      created: created ?? this.created,
+      updated: updated ?? this.updated,
     );
   }
 
   Map<String, dynamic> toJson() => _$ItemIdentifierToJson(this);
+
+  /// This method is a helper method to ensure that
+  /// created and updated can be initialized to equivalent values if
+  /// one or both are null.
+  static DateTime _defaultDateTime(DateTime? dateTime) => dateTime ?? DateTime.now();
 }
