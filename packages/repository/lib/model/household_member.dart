112c9475ca555a7951c9a1c87b0f9e65bc541dfb
Matt Watkins <48530805+watkins-matt@users.noreply.github.com>
2024-01-04 08:57:31 -0800

    Add copyWith and merge

--- packages/repository/lib/model/household_member.dart
+++ packages/repository/lib/model/household_member.dart
@@ -43,5 +43,45 @@ class HouseholdMember {
         userId = userId ?? hashEmail(email);
 
   factory HouseholdMember.fromJson(Map<String, dynamic> json) => _$HouseholdMemberFromJson(json);
+
+  HouseholdMember copyWith({
+    bool? isAdmin,
+    DateTime? timestamp,
+    String? email,
+    String? householdId,
+    String? name,
+    String? userId,
+  }) {
+    return HouseholdMember(
+      isAdmin: isAdmin ?? this.isAdmin,
+      timestamp: timestamp ?? this.timestamp,
+      email: email ?? this.email,
+      householdId: householdId ?? this.householdId,
+      name: name ?? this.name,
+      userId: userId ?? this.userId,
+    );
+  }
+
+  HouseholdMember merge(HouseholdMember other) => _$merge(this, other);
+
   Map<String, dynamic> toJson() => _$HouseholdMemberToJson(this);
+  HouseholdMember _$merge(HouseholdMember first, HouseholdMember second) {
+    // Check if the userIds match. If not, throw an exception.
+    if (first.userId != second.userId) {
+      throw Exception('Cannot merge HouseholdMembers with different userIds.');
+    }
+
+    // Determine which HouseholdMember is newer based on the timestamp.
+    final newerMember = second.timestamp.isAfter(first.timestamp) ? second : first;
+
+    // Merge the fields, preferring the values from the newer member.
+    return HouseholdMember(
+        email: newerMember.email.isNotEmpty ? newerMember.email : first.email,
+        householdId:
+            newerMember.householdId.isNotEmpty ? newerMember.householdId : first.householdId,
+        name: newerMember.name.isNotEmpty ? newerMember.name : first.name,
+        timestamp: newerMember.timestamp,
+        userId: first.userId,
+        isAdmin: newerMember.isAdmin);
+  }
 }
