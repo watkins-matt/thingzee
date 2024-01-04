4ec71e76c1e116490b3dfb259ededa8a55e1ea83
Matt Watkins <48530805+watkins-matt@users.noreply.github.com>
2024-01-03 19:54:38 -0800

    Add deserialize, serialize, tag and uniqueDocumentId

--- packages/repository_appw/lib/util/item_db_new.dart
+++ packages/repository_appw/lib/util/item_db_new.dart
@@ -1,43 +1,29 @@
 import 'package:appwrite/appwrite.dart';
-import 'package:appwrite/models.dart' hide Log, Preferences;
-import 'package:collection/collection.dart';
-import 'package:log/log.dart';
 import 'package:repository/database/item_database.dart';
 import 'package:repository/database/preferences.dart';
 import 'package:repository/model/filter.dart';
 import 'package:repository/model/item.dart';
-import 'package:repository/util/hash.dart';
 import 'package:repository_appw/util/appwrite_database.dart';
 import 'package:repository_appw/util/synchronizable.dart';
 
 class AppwriteItemDatabase extends ItemDatabase
     with AppwriteSynchronizable<Item>, AppwriteDatabase<Item> {
+  static const String TAG = 'AppwriteItemDatabase';
+
   AppwriteItemDatabase(
     Preferences prefs,
     Databases database,
     String databaseId,
     String collectionId,
   ) : super() {
-    constructSynchronizable('AppwriteItemDatabase', prefs, onConnectivityChange: () async {
+    constructSynchronizable(TAG, prefs, onConnectivityChange: () async {
       await taskQueue.runUntilComplete();
     });
-    constructDatabase(database, databaseId, collectionId);
+    constructDatabase(TAG, database, databaseId, collectionId);
   }
 
   @override
-  List<Item> documentsToList(DocumentList documents) {
-    return documents.documents
-        .map((doc) {
-          try {
-            return Item.fromJson(doc.data);
-          } catch (e) {
-            Log.w('Failed to deserialize Item from upc: ${doc.data["upc"]}', e);
-            return null;
-          }
-        })
-        .whereNotNull()
-        .toList();
-  }
+  Item deserialize(Map<String, dynamic> json) => Item.fromJson(json);
 
   @override
   List<Item> filter(Filter filter) {
@@ -48,36 +34,23 @@ class AppwriteItemDatabase extends ItemDatabase
   }
 
   @override
-  List<Item> getChanges(DateTime since) {
-    return values
-        .where((item) => item.lastUpdate != null && item.lastUpdate!.isAfter(since))
-        .toList();
-  }
+  String getKey(Item item) => item.upc;
 
   @override
-  String getKey(Item item) => item.upc;
+  DateTime? getUpdated(Item item) => item.lastUpdate;
 
   @override
   Item merge(Item existingItem, Item newItem) => existingItem.merge(newItem);
 
   @override
-  List<Item> search(String query) {
-    return values.where((item) => item.name.contains(query)).toList();
+  List<Item> search(String string) {
+    return values.where((item) => item.name.contains(string)).toList();
   }
 
   @override
-  Map<String, dynamic> serializeItem(Item item) {
+  Map<String, dynamic> serialize(Item item) {
     var json = item.toJson();
     json['userId'] = userId;
     return json;
-  }
-
-  @override
-  String uniqueDocumentId(String id) {
-    if (userId.isEmpty) {
-      throw Exception('AppwriteItemDB: User ID is empty, cannot generate unique document ID.');
-    }
-
-    return hashBarcode(userId, id);
   }
 }
