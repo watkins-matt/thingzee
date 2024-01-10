a6d72f9f1ce83461f4c9a2e55c4a1356800f24f9
Matt Watkins <48530805+watkins-matt@users.noreply.github.com>
2024-01-09 17:55:59 -0800

    Add model base class

--- packages/repository/lib/database/database.dart
+++ packages/repository/lib/database/database.dart
@@ -1,4 +1,6 @@
-abstract class Database<T> {
+import 'package:repository/model/abstract/model.dart';
+
+abstract class Database<T extends Model> {
   List<T> all();
   void delete(T item);
   void deleteAll();
@@ -7,6 +9,8 @@ abstract class Database<T> {
   List<T> getAll(List<String> ids);
   List<T> getChanges(DateTime since);
   String getKey(T item);
+  bool isEqual(T a, T b);
   Map<String, T> map();
+  T merge(T existingItem, T newItem);
   void put(T item);
 }
