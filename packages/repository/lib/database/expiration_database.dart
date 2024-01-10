08eeb8fc351e7ea1fd7dff01fe4f0e93f72f052c
Matt Watkins <48530805+watkins-matt@users.noreply.github.com>
2024-01-09 17:56:54 -0800

    Inherit from Database

--- packages/repository/lib/database/expiration_database.dart
+++ packages/repository/lib/database/expiration_database.dart
@@ -1,12 +1,19 @@
+// ignore_for_file: annotate_overrides
+
+import 'package:repository/database/database.dart';
 import 'package:repository/model/expiration_date.dart';
 
-abstract class ExpirationDatabase {
+abstract class ExpirationDatabase implements Database<ExpirationDate> {
   List<ExpirationDate> all();
-  void delete(String upc);
+  void delete(ExpirationDate date);
   void deleteAll();
-  DateTime? get(String upc);
-  List<DateTime> getAll(List<String> upcs);
+  void deleteById(String id);
+  ExpirationDate? get(String upc);
+  List<ExpirationDate> getAll(List<String> upcs);
   List<ExpirationDate> getChanges(DateTime since);
+  String getKey(ExpirationDate item);
+  bool isEqual(ExpirationDate a, ExpirationDate b);
   Map<String, ExpirationDate> map();
+  ExpirationDate merge(ExpirationDate existingItem, ExpirationDate newItem);
   void put(ExpirationDate date);
 }
