e8183ff977dffcb37d070b0b71c12a25d3e0d336
Matt Watkins <48530805+watkins-matt@users.noreply.github.com>
2024-02-29 17:05:53 -0800

    Move trackers to base receipt parser

--- lib/pages/receipt_scanner/parser/parser.dart
+++ lib/pages/receipt_scanner/parser/parser.dart
@@ -1,8 +1,19 @@
 import 'package:repository/model/receipt.dart';
 import 'package:repository/model/receipt_item.dart';
+import 'package:thingzee/pages/receipt_scanner/parser/error_corrector.dart';
+import 'package:thingzee/pages/receipt_scanner/parser/ocr_text.dart';
 import 'package:thingzee/pages/receipt_scanner/parser/order_tracker.dart';
+import 'package:thingzee/pages/receipt_scanner/util/frequency_tracker.dart';
 
 abstract class ReceiptParser {
+  final FrequencyTracker<double> totalTracker = FrequencyTracker<double>();
+  final FrequencyTracker<double> subtotalTracker = FrequencyTracker<double>();
+  final FrequencyTracker<double> taxTracker = FrequencyTracker<double>();
+  final FrequencyTracker<DateTime> dateTracker = FrequencyTracker<DateTime>();
+  final FrequencyTracker<double> discountTracker = FrequencyTracker<double>();
+  final ErrorCorrector errorCorrector = ErrorCorrector();
+  final OcrText ocrText = OcrText();
+
   RelativeOrderTracker orderTracker = RelativeOrderTracker();
   String get rawText;
   Receipt get receipt;
