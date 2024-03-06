3233d250b447d12b62825dd10a5536d4efa33b9d
Matt Watkins <48530805+watkins-matt@users.noreply.github.com>
2024-03-05 21:26:34 -0800

    Improve text parser

--- lib/pages/receipt_scanner/parser/element/item_text.dart
+++ lib/pages/receipt_scanner/parser/element/item_text.dart
@@ -2,7 +2,7 @@ import 'package:petitparser/petitparser.dart';
 
 Parser<String> itemTextParser() {
   final textPattern = letter().trim() &
-      (letter() | digit() | pattern(' \'&.')).starGreedy(letter() | newline() | endOfInput()) &
+      (letter() | digit() | pattern(' \'&.-')).starGreedy(letter() | newline() | endOfInput()) &
       (letter() | newline() | endOfInput());
 
   return textPattern.flatten().map((String value) => value.trim());
@@ -16,5 +16,6 @@ Parser<String> skipToItemTextParser() {
   // indicating a potential barcode
   final skipUntilPotentialBarcode = (barcode | any()).starLazy(parser).flatten();
 
-  return (skipUntilPotentialBarcode & parser).map((values) => values[1] as String);
+  return (skipUntilPotentialBarcode & letter().optional() & parser)
+      .map((values) => values[2] as String);
 }
