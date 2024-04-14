98a4a381c4ba0c50e29c7a0e6d6dac5670ea1af0
Matt Watkins <48530805+watkins-matt@users.noreply.github.com>
2024-04-13 18:06:44 -0700

    Remove app bar, streamline user interface

--- lib/pages/shopping/shopping_list_page.dart
+++ lib/pages/shopping/shopping_list_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:receipt_ocr/post_scan_handler.dart';
 import 'package:receipt_ocr/receipt_scanner.dart';
+import 'package:repository/database/shopping_list_database.dart';
 import 'package:repository/model/shopping_item.dart';
 import 'package:thingzee/pages/bottom_nav_bar/state/bottom_nav_state.dart';
 import 'package:thingzee/pages/inventory/state/inventory_view.dart';
@@ -10,66 +12,77 @@ import 'package:thingzee/pages/shopping/confirmation_dialog.dart';
 import 'package:thingzee/pages/shopping/state/shopping_list.dart';
 import 'package:thingzee/pages/shopping/widget/shopping_list_tile.dart';
 
-class ShoppingListPage extends ConsumerWidget {
+final tabIndexProvider = StateProvider<int>((ref) => 0);
+
+class ShoppingListPage extends HookConsumerWidget {
   const ShoppingListPage({super.key});
 
-  Future<void> addItem(BuildContext context, WidgetRef ref) async {
-    ShoppingItem? newItem = await showDialog<ShoppingItem>(
-        context: context,
-        builder: (BuildContext context) {
-          throw UnimplementedError();
-        });
+  Future<void> addNewItem(BuildContext context, WidgetRef ref) async {
+    final shoppingList = ref.read(shoppingListProvider);
+    final items = shoppingList.shoppingItems;
 
-    if (newItem != null) {
-      await ref.read(shoppingListProvider.notifier).add(newItem);
+    // Don't add a new item if the last item is already a new item
+    if (items.isNotEmpty && items.last.name.isEmpty) {
+      return;
     }
+
+    final newItem = ShoppingItem(listName: ShoppingListName.shopping);
+    await ref.read(shoppingListProvider.notifier).add(newItem);
   }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final tabController = useTabController(initialLength: 2);
     ref.watch(shoppingListProvider);
 
+    useEffect(() {
+      tabController.addListener(() {
+        ref.read(tabIndexProvider.notifier).state = tabController.index;
+      });
+      return tabController.dispose;
+    }, [tabController]);
+
     return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        backgroundColor: Theme.of(context).brightness == Brightness.light
-            ? Colors.white
-            : Theme.of(context).scaffoldBackgroundColor,
-        appBar: AppBar(
-          elevation: 0,
-          title: const Text('Shopping'),
-          bottom: const TabBar(
-            tabs: [
-              Tab(text: 'Shopping List'),
-              Tab(text: 'Shopping Cart'),
-            ],
-          ),
-          actions: [
-            IconButton(
-              style: IconButton.styleFrom(
-                  foregroundColor: Theme.of(context).bottomNavigationBarTheme.selectedItemColor),
-              icon: const Icon(Icons.camera_alt),
-              tooltip: 'Scan Receipt',
-              onPressed: () => _navigateToReceiptScannerPage(context),
-            ),
-            TextButton.icon(
-                style: TextButton.styleFrom(
-                    foregroundColor: Theme.of(context).bottomNavigationBarTheme.selectedItemColor),
-                onPressed: () => _handleTripCompleted(context, ref),
-                icon: const Icon(
-                  Icons.check,
+        length: 2,
+        child: Scaffold(
+          backgroundColor: Theme.of(context).brightness == Brightness.light
+              ? Colors.white
+              : Theme.of(context).scaffoldBackgroundColor,
+          body: SafeArea(
+            child: Column(
+              children: <Widget>[
+                Material(
+                  color: Theme.of(context).appBarTheme.backgroundColor,
+                  child: TabBar(
+                    controller: tabController,
+                    labelColor: Theme.of(context).textTheme.bodyLarge?.color,
+                    tabs: const [
+                      Tab(text: 'Shopping List', icon: Icon(Icons.list_alt_outlined)),
+                      Tab(text: 'Shopping Cart', icon: Icon(Icons.shopping_cart_outlined)),
+                    ],
+                  ),
                 ),
-                label: const Text('Done'))
-          ],
-        ),
-        body: TabBarView(
-          children: [
-            shoppingListTab(context, ref),
-            shoppingCartTab(context, ref),
-          ],
-        ),
-      ),
-    );
+                Expanded(
+                  child: TabBarView(
+                    controller: tabController,
+                    children: [
+                      shoppingListTab(context, ref),
+                      shoppingCartTab(context, ref),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          floatingActionButton: ref.watch(tabIndexProvider) == 1 &&
+                  ref.watch(shoppingListProvider).cartItems.isNotEmpty
+              ? FloatingActionButton(
+                  onPressed: () => _handleTripCompleted(context, ref),
+                  tooltip: 'Complete Shopping',
+                  child: const Icon(Icons.check),
+                )
+              : null,
+        ));
   }
 
   Widget shoppingCartItemBuilder(BuildContext context, WidgetRef ref, ShoppingItem item) {
@@ -85,10 +98,21 @@ class ShoppingListPage extends ConsumerWidget {
     final items = shoppingProvider.cartItems;
 
     return items.isEmpty
-        ? const Center(
-            child: Text(
-              'Your shopping cart is empty.',
-              style: TextStyle(fontSize: 18),
+        ? Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  'Your shopping cart is empty.',
+                  style: TextStyle(fontSize: 18),
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton.icon(
+                  icon: const Icon(Icons.camera_alt),
+                  label: const Text('Scan Receipt'),
+                  onPressed: () => _navigateToReceiptScannerPage(context),
+                ),
+              ],
             ),
           )
         : ListView.builder(
@@ -97,8 +121,10 @@ class ShoppingListPage extends ConsumerWidget {
           );
   }
 
-  Widget shoppingListItemBuilder(BuildContext context, WidgetRef ref, ShoppingItem item) {
-    return ShoppingListTile(item: item, editable: true, checkbox: true);
+  Widget shoppingListItemBuilder(
+      BuildContext context, WidgetRef ref, ShoppingItem item, bool isLastItem) {
+    bool shouldAutoFocus = isLastItem && item.name.isEmpty;
+    return ShoppingListTile(item: item, editable: true, checkbox: true, autoFocus: shouldAutoFocus);
   }
 
   Widget shoppingListTab(BuildContext context, WidgetRef ref) {
@@ -112,10 +138,24 @@ class ShoppingListPage extends ConsumerWidget {
               style: TextStyle(fontSize: 18),
             ),
           )
-        : ListView.builder(
-            itemBuilder: (context, index) => shoppingListItemBuilder(context, ref, items[index]),
-            itemCount: items.length,
+        : shoppingListViewBuilder(context, ref, items);
+  }
+
+  Widget shoppingListViewBuilder(BuildContext context, WidgetRef ref, List<ShoppingItem> items) {
+    return ListView.builder(
+      itemCount: items.length + 1,
+      itemBuilder: (context, index) {
+        if (index == items.length) {
+          // This is the "Add new item" tile
+          return ListTile(
+            leading: const Icon(Icons.add),
+            title: const Text('List Item'),
+            onTap: () => addNewItem(context, ref),
           );
+        }
+        return shoppingListItemBuilder(context, ref, items[index], index == items.length - 1);
+      },
+    );
   }
 
   Future<void> _handleTripCompleted(BuildContext context, WidgetRef ref) async {
