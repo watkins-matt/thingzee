6b82cceae61518f9f7ad8f9328738270d21f7e56
Matt Watkins <48530805+watkins-matt@users.noreply.github.com>
2024-07-07 14:42:42 -0700

    Update shopping list tab to use new AnimatedListView

--- lib/pages/shopping/widget/shopping_list_tab.dart
+++ lib/pages/shopping/widget/shopping_list_tab.dart
@@ -2,9 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:repository/database/shopping_list_database.dart';
 import 'package:repository/model/shopping_item.dart';
-import 'package:thingzee/pages/shopping/state/animation.dart';
 import 'package:thingzee/pages/shopping/state/shopping_list.dart';
-import 'package:thingzee/pages/shopping/widget/animated_shopping_list_view.dart';
+import 'package:thingzee/pages/shopping/widget/animated_list_view.dart';
 import 'package:thingzee/pages/shopping/widget/custom_expansion_tile.dart';
 import 'package:thingzee/pages/shopping/widget/shopping_list_tile.dart';
 
@@ -26,7 +25,6 @@ class _ShoppingListTabState extends ConsumerState<ShoppingListTab>
     final shoppingList = ref.read(shoppingListProvider);
     final items = shoppingList.shoppingItems;
 
-    // Don't add a new item if the last item is already a new item
     if (items.isNotEmpty && items.last.name.isEmpty) {
       return;
     }
@@ -55,7 +53,18 @@ class _ShoppingListTabState extends ConsumerState<ShoppingListTab>
             controller: _scrollController,
             child: Column(
               children: [
-                shoppingListViewBuilder(context, ref, uncheckedItems),
+                AnimatedListView<ShoppingItem>(
+                  items: uncheckedItems,
+                  itemBuilder: (context, item) => ShoppingListTile(
+                    item: item,
+                    editable: true,
+                    checkbox: true,
+                    autoFocus: item == uncheckedItems.last && item.name.isEmpty,
+                    onChecked: (uid, checked) {
+                      ref.read(shoppingListProvider.notifier).check(item, checked);
+                    },
+                  ),
+                ),
                 ListTile(
                   leading: const Icon(Icons.add),
                   title: const Text('List Item'),
@@ -71,18 +80,19 @@ class _ShoppingListTabState extends ConsumerState<ShoppingListTab>
     return CustomExpansionTile(
       id: 'ShoppingList.checkedItems',
       title: Text('${checkedItems.length} Checked Items'),
-      children: checkedItems.map((item) {
-        return ShoppingListTile(
-          item: item,
-          editable: false,
-          checkbox: true,
-          onChecked: (uid, checked) {
-            if (!checked) {
-              ref.read(animationStateProvider.notifier).addItem(item);
-            }
-          },
-        );
-      }).toList(),
+      children: [
+        AnimatedListView<ShoppingItem>(
+          items: checkedItems,
+          itemBuilder: (context, item) => ShoppingListTile(
+            item: item,
+            editable: false,
+            checkbox: true,
+            onChecked: (uid, checked) {
+              ref.read(shoppingListProvider.notifier).check(item, checked);
+            },
+          ),
+        ),
+      ],
       onExpansionChanged: (isExpanded) {
         WidgetsBinding.instance.addPostFrameCallback((_) {
           scrollToBottom();
@@ -105,28 +115,5 @@ class _ShoppingListTabState extends ConsumerState<ShoppingListTab>
         curve: Curves.easeOut,
       );
     }
-  }
-
-  Widget shoppingListItemBuilder(
-      BuildContext context, WidgetRef ref, ShoppingItem item, bool isLastItem) {
-    bool shouldAutoFocus = isLastItem && item.name.isEmpty;
-    return ShoppingListTile(item: item, editable: true, checkbox: true, autoFocus: shouldAutoFocus);
-  }
-
-  Widget shoppingListViewBuilder(BuildContext context, WidgetRef ref, List<ShoppingItem> items) {
-    return const AnimatedShoppingListView();
-    // return ListView.builder(
-    //   shrinkWrap: true,
-    //   physics: const NeverScrollableScrollPhysics(),
-    //   itemCount: items.length,
-    //   itemBuilder: (context, index) {
-    //     return shoppingListItemBuilder(
-    //       context,
-    //       ref,
-    //       items[index],
-    //       index == items.length - 1,
-    //     );
-    //   },
-    // );
   }
 }
