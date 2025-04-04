71dd40336672c43a3e95442c6363344a9797bfa6
Matt Watkins <48530805+watkins-matt@users.noreply.github.com>
2025-04-03 18:41:19 -0700

    Use taskfile

--- lib/pages/household/state/invitation_state.dart
+++ lib/pages/household/state/invitation_state.dart
@@ -5,7 +5,9 @@ import 'package:repository/database/invitation_database.dart';
 import 'package:repository/model/invitation.dart';
 import 'package:thingzee/main.dart';
 
-final invitationsProvider = AsyncNotifierProvider<InvitationState, List<Invitation>>(() => InvitationState());
+final invitationsProvider =
+    AsyncNotifierProvider<InvitationState, List<Invitation>>(
+        () => InvitationState());
 
 class InvitationState extends AsyncNotifier<List<Invitation>> {
   @override
@@ -18,33 +20,35 @@ class InvitationState extends AsyncNotifier<List<Invitation>> {
     return await ref.read(cloudRepoProvider.future);
   }
 
-  bool get canSendInvites => state.whenOrNull(
-      data: (_) => true,
-      ) ?? false;
+  bool get canSendInvites =>
+      state.whenOrNull(
+        data: (_) => true,
+      ) ??
+      false;
 
   Future<void> acceptInvite(Invitation invitation) async {
     final cloudRepo = await _getCloudRepo();
-    
+
     // First, mark invitation as processing locally to provide immediate feedback
     state = const AsyncValue.loading();
-    
+
     try {
       Log.i('InvitationState: Accepting invitation ${invitation.uniqueKey}');
-      
+
       // Use the existing accept method, which will trigger database events
       // that the cloud function will react to
       cloudRepo.invitation.accept(invitation);
-      
+
       // Wait a moment to allow the cloud function to process
       await Future.delayed(const Duration(seconds: 1));
-      
+
       // Update the household members after joining a new household
       // We need to reload all data since the household ID changed
       await cloudRepo.fetch();
-      
+
       // Refresh the invitations list
       await refreshInvitations();
-      
+
       Log.i('InvitationState: Invitation accepted successfully');
     } catch (e, stack) {
       Log.e('InvitationState: Exception accepting invitation', e, stack);
@@ -52,27 +56,27 @@ class InvitationState extends AsyncNotifier<List<Invitation>> {
       rethrow;
     }
   }
-  
+
   Future<void> declineInvite(Invitation invitation) async {
     state = const AsyncValue.loading();
-    
+
     try {
       Log.i('InvitationState: Declining invitation ${invitation.uniqueKey}');
-      
+
       final cloudRepo = await _getCloudRepo();
-      
+
       // Create updated invitation with 'rejected' status (not 'declined')
       final updatedInvitation = invitation.copyWith(
         status: InvitationStatus.rejected,
       );
-      
+
       // Update the invitation in the database
       // Since there's no direct 'decline' method, we'll update the invitation directly
       cloudRepo.invitation.put(updatedInvitation);
-      
+
       // Refresh local invitations list
       await refreshInvitations();
-      
+
       Log.i('InvitationState: Invitation declined successfully');
     } catch (e, stack) {
       Log.e('InvitationState: Error declining invitation', e, stack);
@@ -83,7 +87,9 @@ class InvitationState extends AsyncNotifier<List<Invitation>> {
 
   Future<bool> isUserInvited(String email) async {
     final cloudRepo = await _getCloudRepo();
-    if (!(cloudRepo.isMultiUser && cloudRepo.isUserVerified && cloudRepo.loggedIn)) {
+    if (!(cloudRepo.isMultiUser &&
+        cloudRepo.isUserVerified &&
+        cloudRepo.loggedIn)) {
       return false;
     }
     InvitationDatabase invitation = cloudRepo.invitation;
@@ -108,19 +114,24 @@ class InvitationState extends AsyncNotifier<List<Invitation>> {
 
   Future<void> cancelInvitation(Invitation invitation) async {
     state = const AsyncValue.loading();
-    
+
     try {
       Log.i('InvitationState: Canceling invitation ${invitation.uniqueKey}');
-      
+
       final cloudRepo = await _getCloudRepo();
-      
-      // Remove the invitation from the database using deleteById
-      cloudRepo.invitation.deleteById(invitation.uniqueKey);
-      
+
+      // Create updated invitation with 'rejected' status since there's no 'canceled' status
+      final updatedInvitation = invitation.copyWith(
+        status: InvitationStatus.rejected,
+      );
+
+      // Update the invitation in the database
+      cloudRepo.invitation.put(updatedInvitation);
+
       // Refresh local invitations list
       await refreshInvitations();
-      
-      Log.i('InvitationState: Invitation canceled and removed successfully');
+
+      Log.i('InvitationState: Invitation canceled successfully');
     } catch (e, stack) {
       Log.e('InvitationState: Error canceling invitation', e, stack);
       state = AsyncValue.error(e, stack);
@@ -130,7 +141,9 @@ class InvitationState extends AsyncNotifier<List<Invitation>> {
 
   Future<void> sendInvite(String recipientEmail) async {
     final cloudRepo = await _getCloudRepo();
-    if (!(cloudRepo.isMultiUser && cloudRepo.isUserVerified && cloudRepo.loggedIn)) {
+    if (!(cloudRepo.isMultiUser &&
+        cloudRepo.isUserVerified &&
+        cloudRepo.loggedIn)) {
       throw Exception('User cannot send invites in current state');
     }
 
